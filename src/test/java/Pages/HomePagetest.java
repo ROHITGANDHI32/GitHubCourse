309c2faf7a228f317a9bbfb3b9e900c58be1b7f7
ROHITGANDHI32 <ROHITGANDHI32>
2024-04-10 11:23:11 +0530

    My first commit

---
+++ src/test/java/Pages/HomePagetest.java
@@ -0,0 +1,17 @@
+package Pages;
+
+import org.testng.annotations.Test;
+
+public class HomePagetest {
+
+	
+
+	@Test
+	
+	public void test2() {
+		
+		System.out.println("This is HomePage test case");
+	}
+ 	
+	
+}
---
+++ src/test/java/Pages/LoginPage.java
@@ -0,0 +1,16 @@
+package Pages;
+
+import org.testng.annotations.Test;
+
+public class LoginPage {
+
+	
+	@Test
+	
+	public void test1() {
+		
+		System.out.println("This is 1st test case");
+	}
+ 	
+	
+}
